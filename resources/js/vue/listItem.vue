bd1918b567f718fe5b8a156d85476f9c15c37fcd
ericdvlpr <ericjaucian@gmail.com>
2021-03-18 22:59:41 +0800

    Added List view items

--- resources/js/vue/listItem.vue
+++ resources/js/vue/listItem.vue
@@ -1,12 +1,40 @@
 <template>
-    <div>
-       Item View
+    <div class="item">
+       <input 
+            type="checkbox"
+            @change="updateCheck()"
+            
+        />
+       <span >{{item.name}}</span>
+       <button @click="removeItem()" class="trashcan">
+           <font-awesome-icon icon="trash" />
+       </button>
     </div>
 </template>
 <script>
 export default {
-    setup() {
-        
-    },
+    props:['item']
 }
 </script> 
+<style scoped>
+    .completed{
+        text-decoration: line-through;
+        color: #999999;
+    }
+    .itemText{
+        width: 100%;
+        margin-left: 20px;
+    }
+    .item{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+    }
+    .trashcan{
+       background: #e6e6e6;
+       border: none;
+       color:#FF0000;
+       outline:none;
+    }
+</style>
